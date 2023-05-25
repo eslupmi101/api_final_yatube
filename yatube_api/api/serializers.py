efc7c7cd8a33da73821f117a2be05295b5f3549b
eslupmi101 <ajsen-andreev@yandex.ru>
2023-05-25 21:05:49 +0900

    Fix bugs

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -41,6 +41,7 @@ class CommentSerializer(serializers.ModelSerializer):
         slug_field='username',
         read_only=True
     )
+    created = serializers.DateTimeField(source='pub_date')
 
     class Meta:
         model = Comment
