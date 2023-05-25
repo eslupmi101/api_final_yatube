5f53649e5e4219f2d9b542bc1460402aae524905
eslupmi101 <ajsen-andreev@yandex.ru>
2023-05-25 22:40:06 +0900

    Fix

--- yatube_api/api/paginations.py
+++ yatube_api/api/paginations.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 
+
 class CustomLimitOffsetPagination(LimitOffsetPagination):
     def get_paginated_response(self, data):
         return Response(data)
