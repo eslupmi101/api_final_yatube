55b1ab362f5debd59166aa1759233c920fd7a0be
eslupmi101 <ajsen-andreev@yandex.ru>
2023-05-26 00:50:40 +0900

    Fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -41,7 +41,6 @@ class CommentSerializer(serializers.ModelSerializer):
         slug_field='username',
         read_only=True
     )
-    created = serializers.DateTimeField(source='pub_date', read_only=True)
 
     class Meta:
         model = Comment
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Comment, Group, Post, Follow
+from .paginations import CustomLimitOffsetPagination
 from .serializers import (
     CommentSerializer, GroupSerializer,
     PostSerializer, FollowSerializer
@@ -14,6 +15,7 @@ from .serializers import (
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = CustomLimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
