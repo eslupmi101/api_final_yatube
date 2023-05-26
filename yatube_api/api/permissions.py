622c90c66a5d273cb67bd2d61cd19a767edb0457
eslupmi101 <ajsen-andreev@yandex.ru>
2023-05-26 13:54:21 +0900

    Update

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,12 +2,5 @@ from rest_framework import permissions
 
 
 class AuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
-
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
